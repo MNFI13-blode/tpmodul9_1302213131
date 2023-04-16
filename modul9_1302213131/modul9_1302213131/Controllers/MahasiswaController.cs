901ca4dd5a97463e9be5767ba73ef3f641ca4e5c
MNFI13-blode <naufaltaurus24@gmail.com>
2023-04-16 15:45:45 +0700

    Menambahkan class mahasiswa

---
+++ modul9_1302213131/modul9_1302213131/Controllers/MahasiswaController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace modul9_1302213131.Controllers
+{
+    public class MahasiswaController : Controller
+    {
+        // GET: MahasiswaController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MahasiswaController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MahasiswaController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MahasiswaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MahasiswaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MahasiswaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MahasiswaController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MahasiswaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ modul9_1302213131/modul9_1302213131/Mahasiswa.cs
@@ -0,0 +1,13 @@
+﻿namespace modul9_1302213131
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+        public Mahasiswa(string nama, string nim)
+        {
+            Nama = nama;
+            Nim = nim;
+        }
+    }
+}
